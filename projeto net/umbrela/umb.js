7450061bff50cf1ad6d3da8ceb857d2733dfefc8
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-07-09 23:26:25 -0300

    E nhe

---
+++ projeto net/umbrela/umb.js
@@ -0,0 +1,9 @@
+function clicar(){
+    var principal= document.getElementById("principal")
+    var tela = document.getElementById("tela1")
+
+   var a = 2
+   if (a >1){
+    principal = tela
+   }
+}
